df34ebaf0b07f0dc93756ecb0ce40f3430e7c3ca
Mugammad Benjamin <bmugammad@gmail.com>
2024-09-27 17:50:43 +0200

    Login and Registration created

--- app/src/main/java/com/st10079970/prixfinance/MainActivity.kt
+++ app/src/main/java/com/st10079970/prixfinance/MainActivity.kt
@@ -1,20 +1,33 @@
 package com.st10079970.prixfinance
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        // Set padding for system bars
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Find the button and set the click listener to redirect to LoginActivity
+        val btn4Test = findViewById<Button>(R.id.btn4Test)  // Adjust to the correct button ID
+        btn4Test.setOnClickListener {
+            // Launch the LoginActivity when the button is clicked
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
         }
     }
 }
