8bcffaf9bfe86e2ad48900fafe4b900adb6b9741
Teshana <teshanawhite0@gmail.com>
2024-09-30 19:47:11 +0200

    transaction things

--- app/src/main/java/com/st10079970/prixfinance/IncomeFragment.kt
+++ app/src/main/java/com/st10079970/prixfinance/IncomeFragment.kt
@@ -1,7 +1,21 @@
 package com.st10079970.prixfinance
 
+import android.os.Bundle
 import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [IncomeFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
 class IncomeFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
